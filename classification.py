7b6758501692785d4804582ea4ea01604adc00f1
Anuththara <164532630+Dehipitiaya@users.noreply.github.com>
2025-01-13 20:52:35 +0530

    Create classification.py
    create the file

---
+++ classification.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import os
+import cv2
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
